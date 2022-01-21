fcd44b84a18b39c1e64113bd29fcc15d9461eae3
ecrseer <juustino@live.com>
2022-01-21 17:12:33 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,32 @@
+function recurs(nmbr){
+    
+    if (nmbr==1)
+    return 1;
+    else
+    arr.push(3*recurs(nmbr-1));
+}
+
+let 
+    isWhite = false,
+    arr=[];
+    let
+    fixo=new Array(5);
+
+    fixo[0]='E';
+    fixo[4]='S';
+    console.log(fixo);
+
+
+function changeTheme(){    
+    isWhite ? document.querySelector("body").style.backgroundColor='#010101' :
+    document.querySelector("body").style.backgroundColor='#fff';
+
+    isWhite= !isWhite;
+
+    function hapn(){
+        console.log('data');
+    }
+
+    console.log(arr);
+}
+
